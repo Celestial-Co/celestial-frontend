77894c5f49eb0aea332c98f44a70dfc6938a1424
PantheraNestah <pantheranestah@gmail.com>
2025-06-13 13:12:38 +0300

    modified landing page and main App to enhance pages layout and add routes

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import "./pages/pages.scss";
 
 import Landing_page from './pages/Landing';
+import About from './pages/About';
 
 const Landing_page1 = () => {
   return (
@@ -29,11 +34,14 @@ const Landing_page1 = () => {
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Landing_page1 />} />
-        {/* Add more routes here as needed */}
-        <Route path="/landing" element={<Landing_page />} />
-      </Routes>
+      <div className="app_container">
+        <Routes>
+          <Route path="/" element={<Landing_page1 />} />
+          {/* Add more routes here as needed */}
+          <Route path="/landing" element={<Landing_page />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- app/src/pages/Landing.js
+++ app/src/pages/Landing.js
@@ -3,6 +3,8 @@ import "./pages.scss";
 
 import Header from "../components/header";
 import Home_hero from "../components/Home_hero";
+import Footer from "../components/footer";
+import Services_section from "../components/Services_section";
 
 const Landing_page = () => {
 
@@ -11,7 +13,9 @@ const Landing_page = () => {
             <Header />
             <div className="content  mx-auto">
                 <Home_hero />
+                <Services_section />
             </div>
+            <Footer />
         </div>
     );
 }
