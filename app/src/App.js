c7c2ce33352ef5956e9e86f1688ad1dd26c21157
PantheraNestah <pantheranestah@gmail.com>
2025-07-31 17:05:55 +0300

    refactor: clean up App.js and update routing structure
    - Remove unused Landing_page1 component
    - Update main route to use LandingPage component
    - Change stylesheet import to use modular SCSS structure

--- app/src/App.js
+++ app/src/App.js
@@ -5,40 +5,18 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import "./pages/pages.scss";
+import "./styles/main.scss";
 
-import Landing_page from './pages/Landing';
+import LandingPage from './pages/Landing';
 import About from './pages/About';
-
-const Landing_page1 = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 function App() {
   return (
     <Router>
       <div className="app_container">
         <Routes>
-          <Route path="/" element={<Landing_page1 />} />
           {/* Add more routes here as needed */}
-          <Route path="/landing" element={<Landing_page />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
